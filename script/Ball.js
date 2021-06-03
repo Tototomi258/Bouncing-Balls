a0c3de616b6a1a52cd2d4c08632c67b65dcdb542
Tomas Nicolorich <tomasnicolorich@gmail.com>
2021-06-03 17:39:37 -0300

    Added requestAnimationFrame

--- script/Ball.js
+++ script/Ball.js
@@ -1,61 +1,56 @@
 class Ball {
-
   constructor(id, x, y, xSpeed, ySpeed, color, size, container) {
-      this.x = x;
-      this.y = y;
-      this.xSpeed = xSpeed;
-      this.ySpeed = ySpeed;
-      this.size=size;
-      this.width = size;
-      this.height = size;
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.size = size;
+    this.width = size;
+    this.height = size;
 
-      this.container = container;
-  
-      this.element = document.createElement("div");
-      this.element.style.backgroundColor = color;
-      this.element.style.width = this.width + "px";
-      this.element.style.height = this.height + "px";
-      this.element.className += "ball";
-      this.element.style.left = this.x  + "px";
-      this.element.style.top = this.y + "px";
-      this.element.id = id;    
+    this.container = container;
+
+    this.element = document.createElement("div");
+    this.element.style.backgroundColor = color;
+    this.element.style.width = this.width + "px";
+    this.element.style.height = this.height + "px";
+    this.element.className += "ball";
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
+    this.element.id = id;
   }
 
   move() {
-      this.x+=this.xSpeed;
-      this.y+=this.ySpeed;
-      this.changeDirectionIfNecessary(this.x, this.y);
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    this.changeDirectionIfNecessary(this.x, this.y);
 
-      this.element.style.left = this.x  + "px";
-      this.element.style.top = this.y  + "px";
-      // TODO: change to a requestAnimationFrame()
-
+    this.element.style.left = this.x + "px";
+    this.element.style.top = this.y + "px";
   }
-  
-  changeDirectionIfNecessary (x, y) {
-  if (x < 0 || x > this.container.offsetWidth - this.size) {
-  
-    // if we are outside of container we invert the direction
-     this.xSpeed = -this.xSpeed;
-      
-    // if we are outside of container we have to put the ball inside again
-    if(x < 0 ){
-      this.x = 0;
-    } else {
-      this.x = this.container.offsetWidth - this.size;
+
+  changeDirectionIfNecessary(x, y) {
+    if (x < 0 || x > this.container.offsetWidth - this.size) {
+      // if we are outside of container we invert the direction
+      this.xSpeed = -this.xSpeed;
+
+      // if we are outside of container we have to put the ball inside again
+      if (x < 0) {
+        this.x = 0;
+      } else {
+        this.x = this.container.offsetWidth - this.size;
+      }
     }
-  }
-  if (y < 0 || y > this.container.offsetHeight - this.size) {
-  
-    // if we are outside of container we invert the direction
-    this.ySpeed = -this.ySpeed;
+    if (y < 0 || y > this.container.offsetHeight - this.size) {
+      // if we are outside of container we invert the direction
+      this.ySpeed = -this.ySpeed;
 
-    // if we are outside of container we have to put the ball inside again
-    if(y < 0 ){
-      this.y = 0;
-    } else {
-      this.y = this.container.offsetHeight - this.size;
+      // if we are outside of container we have to put the ball inside again
+      if (y < 0) {
+        this.y = 0;
+      } else {
+        this.y = this.container.offsetHeight - this.size;
+      }
     }
-  }
   }
 }
--- script/Container.js
+++ script/Container.js
@@ -1,74 +1,85 @@
 class Container {
+  constructor(element, height, width, ballsUpdatedEvent) {
+    const that = this;
 
-    constructor(element, height, width, ballsUpdatedEvent) {
-        const that=this;
-        
-        this.element = element;
-        this.width = width;
-        this.height = height;
-        this.element.style.width = width;
-        this.element.style.height = height;
+    this.element = element;
+    this.width = width;
+    this.height = height;
+    this.element.style.width = width;
+    this.element.style.height = height;
 
-        this.moved = false;
+    this.moved = false;
 
-        this.timeFrame = 1000/60;
-        this.balls=[];
-        this.defaultBallSize=40;
-        this.colors = ["blue", "red", "green"];
+    this.timeFrame = 1000 / 60;
+    this.balls = [];
+    this.defaultBallSize = 40;
+    this.colors = ["blue", "red", "green"];
 
-        this.clickOnMove=false;
+    this.clickOnMove = false;
 
-        this.ballsUpdatedEvent = ballsUpdatedEvent;
+    this.ballsUpdatedEvent = ballsUpdatedEvent;
 
-        // Check for mousdown and change value of variable "moved" to false
-        element.addEventListener("mousedown", function (e) {
-            that.clickOnMove = false;
-        });
-        // Check for mousemove and change value of variable "moved" to true
-        element.addEventListener("mousemove", function (e) {
-            that.clickOnMove = true;
-        });
-        // If "moved" variable is set to true, then ball direction is equal to mouse movement
-        // If "moved" variable is set to false, then ball direction is random
-        element.addEventListener("mouseup", function (e) {
-            if (that.clickOnMove) {
-                // TODO: Direction based on mouse movement
-                console.log("moved");
-            } else {
-                const genSpeed = () => {
-                    const num = Math.floor(Math.random()*4) + 1; // this will get a number between 1 and 99;
-                    return num * (Math.round(Math.random()) ? 1 : -1);
-                }
-                const containerRect = e.target.getBoundingClientRect();
-                const x = (e.clientX - containerRect.left) - that.defaultBallSize/2; //x position within the element.
-                const y = (e.clientY - containerRect.top) -  that.defaultBallSize/2;//y position within the element.
-                //const x = (e.x - (window.innerWidth - container.width) / 2) + that.ballSize / 2;
-                //const y = (e.y - (window.innerHeight - container.height) / 2)  + that.ballSize / 2;
-                const xSpeed = genSpeed();
-                const ySpeed = genSpeed();
-                const randomNumber = Math.floor(Math.random() * 3);
-                const selectedColor = that.colors[randomNumber];
+    // Check for mousdown and change value of variable "moved" to false
+    element.addEventListener("mousedown", function (e) {
+      that.clickOnMove = false;
+    });
+    // Check for mousemove and change value of variable "moved" to true
+    element.addEventListener("mousemove", function (e) {
+      that.clickOnMove = true;
+    });
+    // If "moved" variable is set to true, then ball direction is equal to mouse movement
+    // If "moved" variable is set to false, then ball direction is random
+    element.addEventListener("mouseup", function (e) {
+      if (that.clickOnMove) {
+        // TODO: Direction based on mouse movement
+        console.log("moved");
+      } else {
+        const genSpeed = () => {
+          const num = Math.floor(Math.random() * 4) + 1; // this will get a number between 1 and 99;
+          return num * (Math.round(Math.random()) ? 1 : -1);
+        };
+        const containerRect = e.target.getBoundingClientRect();
+        const x = e.clientX - containerRect.left - that.defaultBallSize / 2; //x position within the element.
+        const y = e.clientY - containerRect.top - that.defaultBallSize / 2; //y position within the element.
+        //const x = (e.x - (window.innerWidth - container.width) / 2) + that.ballSize / 2;
+        //const y = (e.y - (window.innerHeight - container.height) / 2)  + that.ballSize / 2;
+        const xSpeed = genSpeed();
+        const ySpeed = genSpeed();
+        const randomNumber = Math.floor(Math.random() * 3);
+        const selectedColor = that.colors[randomNumber];
 
-                const ball=new Ball(that.balls.length, x, y, xSpeed, ySpeed, selectedColor, that.defaultBallSize, that.element);
-                that.balls.push(ball);
-            }
-            that.ballsUpdatedEvent(that.balls);
-        });
+        const ball = new Ball(
+          that.balls.length,
+          x,
+          y,
+          xSpeed,
+          ySpeed,
+          selectedColor,
+          that.defaultBallSize,
+          that.element
+        );
+        that.balls.push(ball);
+      }
+      that.ballsUpdatedEvent(that.balls);
+    });
 
-        setInterval(function () {
-            that.element.innerHTML= '';
-            for (let index = 0; index < that.balls.length; index++) {
-                that.balls[index].move();
-                that.element.appendChild(that.balls[index].element);
-                console.log("stop");
-            }
-        }, this.timeFrame );
-    
+    requestAnimationFrame(this.move.bind(this));
+  }
+
+  move() {
+    console.log(this);
+    this.element.innerHTML = "";
+    for (let index = 0; index < this.balls.length; index++) {
+      this.balls[index].move();
+      this.element.appendChild(this.balls[index].element);
+      console.log("stop");
     }
 
-    reset(){
-       this.balls=[];
-       this.ballsUpdatedEvent(this.balls);
-    }
+    requestAnimationFrame(this.move.bind(this));
+  }
 
+  reset() {
+    this.balls = [];
+    this.ballsUpdatedEvent(this.balls);
+  }
 }
