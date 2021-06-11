34aada7d7ffee218f4601e2b609dc2f5389ac988
giacomo <giacomo.mana@outlook.it>
2021-06-11 11:05:08 +0200

    added css comments and resolved last design bugs

--- script/script.js
+++ script/script.js
@@ -67,18 +67,21 @@ document.addEventListener("keydown", function (e) {
 });
 
 
-//music and sound
-const music = document.getElementById("backgroundMusic");   
-const musicButtonPlay = document.getElementById("musicPlay");   
-musicButtonPlay.addEventListener("click", function(){
-  music.play();});
+// music play
+const music = document.getElementById("backgroundMusic");
+const musicButtonPlay = document.getElementById("musicPlay");
+musicButtonPlay.addEventListener("click", function () {
+  music.play();
+});
 
-const musicButtonPause = document.getElementById("musicPause");   
-musicButtonPause.addEventListener("click", function(){
-  music.pause();});
+// music pause
+const musicButtonPause = document.getElementById("musicPause");
+musicButtonPause.addEventListener("click", function () {
+  music.pause();
+});
 
-  //info
+// alert info 
 var info = document.getElementById("info");
-info.addEventListener("click",function(){
-  window.alert("\t\t\tBouncing Balls\n\n\tGame Info:\n\n- Use \"Speed +\" and \"Speed -\" to increase and decrease the balls speeds\n\n- Use \"BallSize +\" and \"BallSize -\" to increase and decrease the balls sizes\n\n- Use \"Pause\" and \"Play\" to stop and resume the game\n\n- Use \"Reset\" to delete all the balls in the container\n\n- Use the palette to change the colours of the balls\n\n- Use the slider to adjust the gravity of the container\n\n- Use \"🎵\" and \"🔇\" to play and pause the music")
+info.addEventListener("click", function () {
+  window.alert("\t\t\tBouncing Balls\n\n\tGame Info:\n\n- Use \"Speed +\" and \"Speed -\" to increase and decrease the balls speeds\n\n- Use \"BallSize +\" and \"BallSize -\" to increase and decrease the balls sizes\n\n- Use \"Pause\" and \"Play\" to stop and resume the game\n\n- Use \"Reset\" to delete all the balls in the container\n\n- Use the palette to change the colours of the balls\n\n- Use the slider to adjust the gravity of the container\n\n- Use \"🎵\" and \"🔇\" to play and pause the music");
 });
